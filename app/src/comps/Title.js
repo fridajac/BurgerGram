7e603e54fcf84829078f4c4620cbc7f66dcff722
fridajac <frida_jacobsson@live.se>
2022-03-24 10:07:20 +0100

    Created a React project

---
+++ app/src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>BurgerGram</h1>
+      <h2>Your pictures</h2>
+      <p>Lorem ipsum dolor sit amet</p>
+    </div>
+  );
+};
+
+export default Title;
---
+++ app/src/firebase/config.js
@@ -0,0 +1,14 @@
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPBTmZTUrjj3cPzlqRNW1jnlbIb9f3dbM",
+  authDomain: "burgergram-a86e2.firebaseapp.com",
+  projectId: "burgergram-a86e2",
+  storageBucket: "burgergram-a86e2.appspot.com",
+  messagingSenderId: "202635197325",
+  appId: "1:202635197325:web:62c7656354ba2f83211bc4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
