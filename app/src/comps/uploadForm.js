4910135d187606486a745b0c2ffaf628a85c0a26
fridajac <frida_jacobsson@live.se>
2022-03-25 08:51:30 +0100

    Added style and span for image input

--- app/src/comps/uploadForm.js
+++ app/src/comps/uploadForm.js
@@ -18,7 +18,10 @@ const UploadForm = () => {
   };
   return (
     <form>
-      <input type="file" onChange={changeHandler} />
+      <label>
+        <input type="file" onChange={changeHandler} />
+        <span>+</span>
+      </label>
 
       <div className="output">
         {error && <div className="error">{error}</div>}
