89e4bdb22612b4cc7fcbbd7801ef980eea4204fe
fridajac <frida_jacobsson@live.se>
2022-03-25 08:37:31 +0100

    Made progress bar work by using useEffect

--- app/src/comps/progressBar.js
+++ app/src/comps/progressBar.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useStorage from "../hooks/useStorage";
 
 const ProgessBar = ({ file, setFile }) => {
   const { url, progress } = useStorage(file);
-  console.log(progress, url);
+  useEffect(() => {
+    if (url) setFile(null);
+  }, [url, setFile]);
+
   return <div className="progress-bar" style={{ width: progress + "%" }}></div>;
 };
 export default ProgessBar;
